61728bf27508a13898c57da143047c21d822afeb
Bach Ta <tsbach15@gmail.com>
2022-08-20 16:02:07 -0400

    add MUI SignInSide template

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,23 +2,28 @@ import * as React from 'react'
 import { FC, useState } from 'react'
 import axios from 'axios'
 import AuthController from '../controllers/authController'
+import SignInSide from './MUI/SignInSide'
 
 const authController = new AuthController()
 
 const Login: FC = () => {
-  return (
-    <button
-      onClick={() => {
-        authController.registerUser({
-          userEmail: 'test3@test.com',
-          username: 'test3',
-          password: 'testpw3',
-        })
-      }}
-    >
-      Login
-    </button>
-  )
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    })
+
+    authController.registerUser({
+      userEmail: 'test3@test.com',
+      username: 'test3',
+      password: 'testpw3',
+    })
+  }
+
+  return <SignInSide handleSubmit={handleSubmit} />
 }
 
 export default Login
